361dec60a08b0e8e60f04c92f8da0fac0c823ca3
regalconcepts <naboafo18@gmail.com>
2023-10-06 05:44:48 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A C program that prints a line to the standard error
- * Return: Always 1 (Success)
+ *  * main - A C program that prints a line to the standard error
+ *   * Return: Always 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
