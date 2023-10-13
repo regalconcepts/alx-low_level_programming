934ddec4595ed9f7dfad2c8db37b8c37c5eabef6
regalconcepts <naboafo18@gmail.com>
2023-10-12 17:57:10 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function checks for a digit (0 to 9)
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}
