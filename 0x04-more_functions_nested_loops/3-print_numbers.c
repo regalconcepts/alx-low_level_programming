a51757e23709d8fa101294477a3fd9557d6059f7
regalconcepts <naboafo18@gmail.com>
2023-10-12 17:40:26 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints digits (0 to 9)
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar (c);
+	}
+		_putchar('\n');
+}
