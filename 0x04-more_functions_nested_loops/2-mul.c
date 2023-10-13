6c8e0d1166c0b810a07803a6938611650a9ac2c5
regalconcepts <naboafo18@gmail.com>
2023-10-12 17:26:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that mul two int
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
