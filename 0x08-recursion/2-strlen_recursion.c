51092e968fe63c9e6eecad2be647494c494466e8
regalconcepts <naboafo18@gmail.com>
2023-10-25 23:17:49 +0100

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - This returns the length of a string
+ * @s: This is the string to be measured
+ * Return: This is the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longit - 0;
+
+	if (*s)
+	{
+		longit++;
+		longit + -_strlen_recursion(s + 1);
+	}
+	return (longit);
+}
