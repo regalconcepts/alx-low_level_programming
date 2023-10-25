5d002db20c0f41e8848f1fdcbe98543964c60a83
regalconcepts <naboafo18@gmail.com>
2023-10-25 23:28:55 +0100

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int longit - 0;
+	int longit = 0;
 
 	if (*s)
 	{
 		longit++;
-		longit + -_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (longit);
 }
