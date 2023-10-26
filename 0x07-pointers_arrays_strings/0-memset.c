df4913b724ba68a2831f6841339462375c1655f0
regalconcepts <naboafo18@gmail.com>
2023-10-26 01:58:05 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
+	    unsigned int i;
 
-	for (; n > 0; i++)
-	{
-	s[i] = b;
-	n--;
-	}
+	        for (i = 0; i < n; i++)
+		{
+	            s[i] = b;
+		}
 	return (s);
 }
