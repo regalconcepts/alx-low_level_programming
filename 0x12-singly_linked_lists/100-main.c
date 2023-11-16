a5387ecb053adbc9c2fe925e5a9167db0d5c09df
regalconcepts <naboafo18@gmail.com>
2023-11-16 22:59:50 +0000

    update

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, ");
+	printf("challenges one to a race.)\n");
+	return (0);
+}
